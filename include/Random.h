8dda1d7295da1baefdfa5e65afac255fcca6a525
EdgeBlade <danieltoshio@uol.com.br>
2014-05-22 12:15:48 -0300

    Update README.md

---
+++ include/Random.h
@@ -0,0 +1,33 @@
+/*------------------------------------------------------------------*
+ |                                                                  |
+ |  Funções que cuidam de geração (pseudo) aleatória de dados, por  |
+ |  meio de distribuições de probabilidade.                         |
+ |                                                                  |
+ *------------------------------------------------------------------*/
+
+#ifndef RANDOM_H
+#define RANDOM_H
+
+/*-------------------------*
+ |   P R O T Ó T I P O S   |
+ *-------------------------*/
+
+/*
+ *  Recebe dois inteiros tais que a <= b.
+ *  Devolve um inteiro uniformemente no intervalo [a,b].
+ */
+int uniforme(int a, int b);
+
+/*
+ *  Recebe dois valores reais tais que a <= b.
+ *  Devolve um número real uniformemente no intervalo [a,b).
+ */
+double uniformeD(double a, double b);
+
+/*
+ *  Gera um número real sob uma distribuição Normal com 
+ *  média e desvio padrão estipulados.
+ */
+double normal(double media, double desvpadr);
+
+#endif
