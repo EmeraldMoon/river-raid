4c71fc4328431535d637fb9908b7f0328acac317
Gustavo Chicato <gustavo.wandeur@usp.br>
2016-11-03 01:43:54 -0200

    Quem precisa de funções aleatórias quando se tem a STL?

--- src/random.cpp
+++ src/random.cpp
@@ -1,34 +1,24 @@
-#include <cstdlib>  /* rand */
-#include <cmath>    /* sqrt, log */
+#include <random>
+
+/*-------------------------*
+ |   D E F I N I Ç Õ E S   |
+ *-------------------------*----------------------------------------*/
+
+/* Objeto do gerador de nºs aleatórios */
+std::default_random_engine generator;
 
 /*-------------------*
  |   F U N Ç Õ E S   |
  *-------------------*----------------------------------------------*/
 
 double uniforme(double a, double b)
-{
-    double x = (double) rand()/RAND_MAX;
-    return (x * (b - a)) + a;
+{   
+    std::uniform_real_distribution<> distribution(a, b);
+    return distribution(generator);
 }
 
-/*------------------------------------------------------------------*
- *
- *  O código a seguir foi inspirado na biblioteca StdRandom 
- *  (em java) do livro "Algorithms", de Sedgewick & Wayne.
- *
- */
 double normal(double media, double desvpadr)
 {
-    double r, x, y;
-    do {
-        x = uniforme(-1.0, 1.0);
-        y = uniforme(-1.0, 1.0);
-        r = x*x + y*y;
-    } while (r >= 1 or r == 0);
-    
-    /* Valor gerado por uma Normal(0, 1) */
-    r = x * sqrt((-2) * log(r)/r);
-
-    /* Aplicamos média e desvio-padrão */
-    return media + (desvpadr * r);
+    std::normal_distribution<> distribution(media, desvpadr);
+    return distribution(generator);
 }
